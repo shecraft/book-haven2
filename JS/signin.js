b3306032f11172e3aac5926a0d631cc8125f5a51
ojoyetunde@gmail.com <ojoyetunde432@gmail.com>
2025-03-23 14:30:05 +0100

    shecraft pushed this

---
+++ JS/signin.js
@@ -0,0 +1,49 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmw4cU8PJ_Fjl5-OueqW7j-NUrvuTDgck",
+    authDomain: "book-haven-47eeb.firebaseapp.com",
+    projectId: "book-haven-47eeb",
+    storageBucket: "book-haven-47eeb.firebasestorage.app",
+    messagingSenderId: "186873485955",
+    appId: "1:186873485955:web:dcd46f0b2a893b5fc299b6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app)
+
+
+signInForm.addEventListener("submit", logInUser)
+async function logInUser(e) {
+    try {
+        errorP.textContent = ""
+        e.preventDefault()
+        const userDeatails = {
+            email: signInForm.email.value.trim(),
+            password: signInForm.password.value.trim(),
+        }
+        await signInWithEmailAndPassword(auth, userDeatails.email, userDeatails.password)
+        alert("sign In successful")
+    } catch (error) {
+        console.log(error.message);
+        if (error.message === "Firebase: Error (auth/invalid-credential).") {
+            errorP.textContent = "*Invalid email/password."  
+        }
+    }
+}
+onAuthStateChanged(auth, (user)=>{
+    if (user) {
+        console.log(user);
+        
+    }else{
+        console.log("No one is signed In");
+        
+    }
+})
---
+++ JS/signup.js
@@ -0,0 +1,75 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getFirestore, collection, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmw4cU8PJ_Fjl5-OueqW7j-NUrvuTDgck",
+    authDomain: "book-haven-47eeb.firebaseapp.com",
+    projectId: "book-haven-47eeb",
+    storageBucket: "book-haven-47eeb.firebasestorage.app",
+    messagingSenderId: "186873485955",
+    appId: "1:186873485955:web:dcd46f0b2a893b5fc299b6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app)
+  const userColRef = collection(db, "BOOK HAVEN");
+
+
+signUpForm.addEventListener("submit", createSignUpAccount);
+async function createSignUpAccount(e) {
+   try {
+     e.preventDefault();
+     spinner.classList.remove("d-none")
+     submitBTN.disabled = true
+     errorP.textContent ="";
+     const userDetails = {
+        name:signUpForm.fullName.value.trim(),
+        email:signUpForm.email.value.trim(),
+        password:signUpForm.password.value.trim(),
+     }
+     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+     if (!emailRegex.test((userDetails.email))) {
+        throw new Error("*Invaild Email Address*");
+        
+     }
+     if (userDetails.password.length < 6) {
+        throw new Error("*Your password should be at least 6 characters*");
+     }
+     const {email,password, ...details} = userDetails;
+     const res = await createUserWithEmailAndPassword(auth, email, password);
+     alert("Sign Up successfully!!") 
+     const docRef = doc(userColRef, res.user.uid)
+     const docRes = await setDoc(docRef, details);
+     console.log(res); 
+     location.href = `./sign in.html`
+    } catch (error) {
+      if (error.message === "Firebase: Error (auth/email-already-in-use).") {
+        errorP.textContent = "Email already exist";
+        return;
+      }
+     console.log(error);
+     errorP.textContent += error.message;
+    } finally{
+      spinner.classList.add("d-none")
+      submitBTN.disabled = false
+   }
+    
+    
+}
+submitBTN.addEventListener("submit", showAlert);
+function showAlert() {
+  Swal.fire({
+    title: "Hello!",
+    text: "Signed Up successfully",
+    icon: "success",
+    confirmButtonText: "OK"
+});
+}
